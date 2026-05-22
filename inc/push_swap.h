3fe2a16da3048cfe0d37306c95c8b22534723ae3
Pficcare <pficcare@student.42lisboa.com>
2026-05-22 17:08:15 +0100

    remove unused structure

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,24 +6,12 @@
 /*   By: pficcare <pficcare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 18:59:57 by adaferna          #+#    #+#             */
-/*   Updated: 2026/05/22 16:09:05 by pficcare         ###   ########.fr       */
+/*   Updated: 2026/05/22 17:07:27 by pficcare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
-/*---- Structure----*/
-
-typedef struct s_node
-{
-	int				nb;
-	int				index;
-
-	struct s_node	*next;
-	struct s_node	*prev;
-
-}					t_node;
 
 /*----Function Prototype----*/
 
@@ -32,6 +20,5 @@ int					ft_isdigit(char c);
 int					ft_strcmp(char *s1, char *s2);
 int					ft_selector(char *str);
 
-/*----XXX----*/
 
 #endif
