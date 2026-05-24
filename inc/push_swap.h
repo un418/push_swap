7633ae0b5223da521388783926672630aad2d1ca
Adam Fernandes <adaferna@student.42lisboa.com>
2026-05-24 18:24:27 +0100

    feat : add main in push_swap.c & fix build via makefile

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,25 +6,30 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 18:59:57 by adaferna          #+#    #+#             */
-/*   Updated: 2026/05/24 16:27:11 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/05/24 18:16:46 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+// for write()
+# include <unistd.h>
+
+// printf()
+# include <stdio.h> // debug to remove later
+
 /*----Function Prototype----*/
 
 // utils
 
-int			is_valid_number(char *str);
-int			is_digit(char c);
-int			is_str_eq(char *s1, char *s2);
+int			is_valid_number(const char *str);
+int			is_digit(const char c);
+int			is_str_eq(const char *s1, const char *s2);
 
 // parser
 
-int			flags_parser(char *arg);
-int			flags_check(int ac, char **av);
-
+int			flags_parser(const char *arg);
+int			arg_validate(const char **argv);
 
 #endif
