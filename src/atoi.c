70251dabd43a447dbf9cf90be265636a955e5a4a
Adam Fernandes <adaferna@student.42lisboa.com>
2026-05-26 19:59:01 +0100

    chore: fix norm

--- src/atoi.c
+++ src/atoi.c
@@ -2,7 +2,7 @@
 
 long	ft_atoi(const char *str)
 {
-	int	neg;
+	int		neg;
 	long	res;
 
 	neg = 1;
@@ -13,7 +13,7 @@ long	ft_atoi(const char *str)
 			neg *= -1;
 		str++;
 	}
-	while(*str)
+	while (*str)
 	{
 		res = (res * 10) + (*str - '0');
 		str++;
@@ -23,27 +23,8 @@ long	ft_atoi(const char *str)
 }
 
 int	in_int_limits(const char *str)
-{	
-
+{
 	if (ft_atoi(str) < INT_MIN || ft_atoi(str) > INT_MAX)
 		return (0);
 	return (1);
 }
-
-
-
-/* 
-#include <stdio.h>
-
-int	main(void)
-{
-	int	res;
-	char	*nb = "2147483647";
-
-	res = 0;
-	res = ft_atoi(nb);
-	printf("Nb = %d\n", res);
-
-	return (0);
-}
- */
