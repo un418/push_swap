57d54ecb55f7f42652fa30d27a9bbf32827ccf8a
Pficcare <pficcare@student.42lisboa.com>
2026-06-05 13:16:48 +0100

    Add comment for clarity

--- AlphaProgram/wich_one.c
+++ AlphaProgram/wich_one.c
@@ -11,13 +11,7 @@ void	wich_one(t_node **stack_a, t_node **stack_b, int size, t_ctx *ctx)
         return (sort_3(stack_a));
     if (size <= 5)
         return (sort_5(stack_a, stack_b));
-     if (ctx->mode == 6)
-        return ;
-    //      sort_adaptive(stack_a, stack_b, size);
-    // else if (ctx->mode == 2)
-    //     sort_simple(stack_a, stack_b, size);
-    // else if (ctx->mode == 3)
-    //     sort_medium(stack_a, stack_b, size);
-    // else if (ctx->mode == 4)
-    //     sort_complex(stack_a, stack_b, size);
+    if (ctx->mode == 6)
+        return ; // for test and avoid compilation error
+   // to add, all the sort_algorithms and the adaptive mode.
 }
