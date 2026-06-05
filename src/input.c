dc7a2fa8b628fd22c1167c970bfa44235d9890e3
Adam Fernandes <adaferna@student.42lisboa.com>
2026-06-05 11:13:37 +0100

    refacto: implement context in function that need it.

--- src/input.c
+++ src/input.c
@@ -1,47 +1,60 @@
 #include "push_swap.h"
 
-// return mode or 0 for invalid flag
-int	parse_flag(const char *flag)
+// success: return 1
+// error: return 0
+int	parse_flag(const char *flag, t_ctx *ctx)
 {
-	int static	mode;
 
 	// this check is redudant with the caller function
 	// but it improve code readability
 	if (*flag == '-' && *(flag + 1) == '-')
 		flag += 2;
-	if (mode <= 0 && is_str_eq(flag, "adaptive"))
-		mode += 1;
-	else if (mode <= 0 && is_str_eq(flag, "simple"))
-		mode += 2;
-	else if (mode <= 0 && is_str_eq(flag, "medium"))
-		mode += 3;
-	else if (mode <= 0 && is_str_eq(flag, "complex"))
-		mode += 4;
-	else if (mode <= 4 && is_str_eq(flag, "bench"))
-		mode += 10;
+	if (!ctx->mode && is_str_eq(flag, "adaptive"))
+		ctx->mode = 1;
+	else if (!ctx->mode  && is_str_eq(flag, "simple"))
+		ctx->mode = 2;
+	else if (!ctx->mode  && is_str_eq(flag, "medium"))
+		ctx->mode = 3;
+	else if (!ctx->mode  && is_str_eq(flag, "complex"))
+		ctx->mode = 4;
+	else if (!ctx->bench  && is_str_eq(flag, "bench"))
+	{
+		ctx->bench = 1;
+		if (!ctx->mode )
+			ctx->mode = 1;
+	}
 	else
-		return (0);
-	return (mode);
+		return (ctx->mode = 0, 0);
+	return (1);
 }
 
-// return mode or 0 if input invalid
-int	input_validate(const char **argv)
+void init_ctx(t_ctx *ctx)
 {
-	int		i;
-	int		mode;
-	int		*parsed;
+	ctx->mode = 0;
+	ctx->bench = 0;
+	ctx->parsed = NULL;
+	ctx->parsed_size = 0;
+}
+
+// success: return 1
+// error: return 0
+int	input_validate(const char **argv, t_ctx *ctx)
+{
+	int	i;
+	int	ret;
 
 	i = 1;
-	mode = 1;
+	if (!is_flag_prefix(argv[i]))
+		ctx->mode = 1;
 	while (argv[i] && is_flag_prefix(argv[i]) && i < 3)
 	{
-		mode = parse_flag(argv[i++]);
-		if (mode == 0)
+		ret = parse_flag(argv[i++], ctx);
+		if (!ret)
 			return (write (2, "Error\n", 6), 0);
 	}
-	parsed = parse_number(&argv[i]);
-	if (!parsed)
+	ret = parse_number(&argv[i], ctx);
+	if (!ret)
 		return (write (2, "Error\n", 6), 0);
-	//need to free parsed later
-	return (mode);
+	//need to free ctx->parsed later
+	return (1);
 }
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 19:03:20 by adaferna          #+#    #+#             */
-/*   Updated: 2026/05/25 00:55:55 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/06/05 01:53:40 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int	main(int argc, const char **argv)
 {
-	int	mode;
+	t_ctx	ctx;
+	int		ret;
 
+	init_ctx(&ctx);
 	if (argc == 1)
 		return (0);
-	mode = input_validate(argv);
-	if (mode == 0)
+	ret = input_validate(argv, &ctx);
+	if (!ret || !ctx.mode)
 		return (1);
-	printf("Mode = %d\n", mode); // Debug to remove later
+	printf("Mode = %d\n", ctx.mode); // Debug to remove later
 	return (0);
 }
