7b9596958ee88a3fff1a3519ba7aa9e9bfc7e524
Pficcare <pficcare@student.42lisboa.com>
2026-06-07 16:40:40 +0100

    Add is sorted function

--- AlphaProgram/wich_one.c
+++ AlphaProgram/wich_one.c
@@ -1,10 +1,28 @@
 #include "push_swap.h"
 
+static int	is_sorted(t_node *head)
+{
+	t_node	*check;
+
+	if (!head)
+		return (1);
+	check = head;
+	while (1)
+	{
+		if (check->index > check->next->index)
+			return (0);
+		check = check->next;
+		if (check == head)
+			break ;
+	}
+	return (1);
+}
+
 void	wich_one(t_node **stack_a, t_node **stack_b, int size, t_ctx *ctx)
 {
     float    dis;
 
-    if (size <= 1)
+    if (size <= 1 || is_sorted(*stack_a))
         return ;
     if (size == 2 && (*stack_a)->index > (*stack_a)->next->index)
         return (swap_a(stack_a, ctx));
