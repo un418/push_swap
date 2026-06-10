4ce4459752bcdad7d7063c032a436138dfe47b05
Adam Fernandes <adaferna@student.42lisboa.com>
2026-06-10 14:38:55 +0100

    improve draft

--- src/bench_mode.c
+++ src/bench_mode.c
@@ -13,8 +13,8 @@
 #include "push_swap.h"
 
 // To do:
+// Done - Implement Submode to print the adaptive selection. can base on disorder
 // Use ft_printf for stderr
-// Implement Submode to print the adaptive selection. can base on disorder
 // Manage float rounding and printing via int
 
 static char *strategy_name(t_ctx *ctx)
@@ -43,14 +43,14 @@ static char *strategy_name(t_ctx *ctx)
 
 void	print_bench(t_ctx *ctx)
 {
-	printf("[bench] disorder:   %f\n", ctx->disorder);
-	printf("[bench] strategy:   %s\n", strategy_name(ctx->mode));
-	printf("[bench] total_ops:   %d\n", ctx->stats.total);
-	printf("[bench] sa:   %d   ", ctx->stats.sa);
-	printf("sb:   %d   ss:   %d   ", ctx->stats.sb, ctx->stats.ss);
-	printf("pa:   %d   pb:   %d\n", ctx->stats.pa, ctx->stats.pb);
-	printf("[bench] ra:   %d   ", ctx->stats.ra);
-	printf("rb:   %d   rr:   %d   ", ctx->stats.rb, ctx->stats.rr);
-	printf("rra:   %d   rrb:   %d", ctx->stats.rra, ctx->stats.rrb);
-	printf("rrr:   %d\n", ctx->stats.rrr);
+	dprintf(2, "[bench] disorder:   %f\n", ctx->disorder);
+	ft_dprintf(2, "[bench] strategy:   %s\n", strategy_name(ctx->mode));
+	ft_dprintf(2, "[bench] total_ops:   %d\n", ctx->stats.total);
+	ft_dprintf(2, "[bench] sa:   %d   ", ctx->stats.sa);
+	ft_dprintf(2, "sb:   %d   ss:   %d   ", ctx->stats.sb, ctx->stats.ss);
+	ft_dprintf(2, "pa:   %d   pb:   %d\n", ctx->stats.pa, ctx->stats.pb);
+	ft_dprintf(2, "[bench] ra:   %d   ", ctx->stats.ra);
+	ft_dprintf(2, "rb:   %d   rr:   %d   ", ctx->stats.rb, ctx->stats.rr);
+	ft_dprintf(2, "rra:   %d   rrb:   %d", ctx->stats.rra, ctx->stats.rrb);
+	ft_dprintf(2, "rrr:   %d\n", ctx->stats.rrr);
 }
--- test/test_utf.c
+++ test/test_utf.c
@@ -1,13 +1,22 @@
 /* File to test UFT-8 output o nterminal */
 
+// run : cc test_utf.c -I../lib/ft_printf/inc ../lib/ft_printf/libftprintf.a && ./a.out
+
 #include <stdio.h>
+#include "ft_printf.h"
 
 int main(int argc, char const *argv[])
 {
 	printf("Simple / O(n\xc2\xb2)");printf("\n");
 	printf("Medium / O(n\xe2\x88\x9an)");printf("\n");
-	printf("O(n2)");printf("\n");
+	printf("O(n²)");printf("\n");
 	printf("O(n√n)");printf("\n");
 	printf("O(n log n)");printf("\n");
+
+	ft_dprintf(FT_STDERR, "Simple / O(n\xc2\xb2)");ft_dprintf(FT_STDERR, "\n");
+	ft_dprintf(FT_STDERR, "Medium / O(n\xe2\x88\x9an)");ft_dprintf(FT_STDERR, "\n");
+	ft_dprintf(FT_STDERR, "O(n²)");ft_dprintf(FT_STDERR, "\n");
+	ft_dprintf(FT_STDERR, "O(n√n)");ft_dprintf(FT_STDERR, "\n");
+	ft_dprintf(FT_STDERR, "O(n log n)");ft_dprintf(FT_STDERR, "\n");
 	return 0;
 }
